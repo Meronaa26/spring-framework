50344d8caeffc9cadb5d21b4e6e28be6e377f70d
merry teweldemedhen <meronaa26@gmail.com>
2022-03-18 10:45:22 -0700

    ading some notes

---
+++ Spring-09-Spring-Thymleafe/src/main/java/com/cydeo/Spring09SpringThymleafeApplication.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring09SpringThymleafeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Spring09SpringThymleafeApplication.class, args);
+    }
+
+}
